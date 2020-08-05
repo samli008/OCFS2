e9e12576bb6badafac6d12e9a3832543064fbd15
liyang <235938019@qq.com>
2020-08-05 03:13:36 -0400

    liyang

--- lvm.sh
+++ lvm.sh
@@ -3,9 +3,16 @@ vgcreate vg1 /dev/vdb
 lvcreate -n lv1 -L 5g vg1
 
 cat >> /etc/rc.local << EOF
+sleep 10
 drbdadm primary drbd0
+sleep 5
+mount -a
 EOF
+
 chmod 755 /etc/rc.local
+
+mkdir /nfs
+echo "/dev/drbd0  /nfs ocfs2 rw,_netdev 0 0" >> /etc/fstab
 
 cat > /etc/exports << EOF
 /nfs 192.168.6.0/24(rw,no_root_squash,async)
